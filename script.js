b688578c53c338dcbb7a4fff75dc39d4094b6b9a
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-03-18 22:06:02 -0400

    part 2

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-var inviteUser = prompt("Would you like to play a game with two characters?");
+var inviteUser = null;
+var userName = null;
 var userWins = 0;
 var userHealth = 40;
 var grantHealth = 10;
 
-if (inviteUser.toLowerCase() === "yes") {
-	var userName = prompt("What would you like to name your character?");
+function startGame() {
+	inviteUser = prompt("Would you like to play a game with two characters?");
+	if (inviteUser.toLowerCase() === "yes") {
+		userName = prompt("What would you like to name your character?");
+		startCombat();
+	}
+}
+
+function startCombat() {
 	while (userHealth > 0 && userWins < 3) {
-		var grantDamage = Math.floor((Math.random() * 2) + 1);
-		var userDamage = Math.floor((Math.random() * 2) + 1);
-		grantHealth -= grantDamage;
-		userHealth -= userDamage;
-		console.log(userName + " has " + userHealth + " health left.");
-		console.log("Grant the almighty has " + grantHealth + " health left.");
-		if (grantHealth <= 0) {
-			userWins += 1;
-			grantHealth = 10;
-			// console.log("grant's health reset to " + grantHealth);
-			console.log(userName + " has " + userWins + " wins!");
+		var takeTurn = prompt("Would you like to attack or quit?");
+		if (takeTurn.toLowerCase() === "attack") {
+			grantHealth -= getDamage();
+			userHealth -= getDamage();
+			console.log(userName + " has " + userHealth + " health left.");
+			console.log("Grant the almighty has " + grantHealth + " health left.");
+			if (grantHealth <= 0) {
+				userWins += 1;
+				grantHealth = 10;
+				// console.log("grant's health reset to " + grantHealth);
+				console.log(userName + " has " + userWins + " wins! You need " + (3 - userWins) + " more wins!");
+			}
+			if (userHealth <= 0) {
+				console.log("Grant is the winner!");
+			}
+			if (userWins >= 3) {
+				console.log(userName + " is the winner!");
+			}
+		} else if (takeTurn.toLowerCase() === "quit") {
+			return;
 		}
-	if (userHealth <= 0) {
-		console.log("Grant is the winner!");
 	}
-	if (userWins >= 3) {
-		console.log(userName + " is the winner!");
-	}
-	}	
-};
+}
+
+function getDamage() {
+	return damage = Math.floor((Math.random() * 2) + 1);
+}
+
+startGame();
