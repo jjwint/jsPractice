9af08fb09f0f90342ce250e1830da553c84a5d7c
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-03-19 20:12:41 -0400

    part 4 pre progress bars

--- script.js
+++ script.js
@@ -23,49 +23,67 @@ grantObj.generateAttackDamage = function() {
 
 var inviteUser = null;
 
+
+document.getElementById("startBtn").addEventListener("click", function() {
+	document.getElementById("startBtn").classList.add("hidden");
+	startGame();
+});
+
 function startGame() {
 	inviteUser = prompt("Would you like to play a game with two characters?");
 	if (inviteUser.toLowerCase() === "yes") {
 		userObj.name = prompt("What would you like to name your character?");
-		console.log(userObj.name);
-		startCombat();
+		displayStats();
 	}
 }
 
-function startCombat() {
-	while ((userObj.health) > 0 && userObj.wins < 5) {
-		var takeTurn = prompt("Would you like to attack, heal, or quit?");
-		if (takeTurn.toLowerCase() === "attack") {
-			grantObj.health -= userObj.generateAttackDamage();
-			userObj.health -= grantObj.generateAttackDamage();
-			console.log(userObj.name + " has " + userObj.health + " health left.");
-			console.log("Grant the almighty has " + grantObj.health + " health left.");
-			if (grantObj.health <= 0) {
-				userObj.wins += 1;
-				grantObj.health = 10;
-				// console.log("grant's health reset to " + grantHealth);
-				console.log(userObj.name + " has " + userObj.wins + " wins! You need " + (3 - userObj.wins) + " more wins!");
-			}
-			if (userObj.health <= 0) {
-				console.log("Grant is the winner!");
-			}
-			if (userObj.wins >= 5) {
-				console.log(userObj.name + " is the winner!");
-			}
-		} else if (takeTurn.toLowerCase() === "heal") {
-			if (userObj.healsRemaining > 0) {
-				userObj.health += userObj.heal();
-				console.log("heals remaining: " + userObj.healsRemaining);
-				console.log("user health: " + userObj.health);
-				// console.log(userObj.name + " was healed "  + " health points.");
-				// console.log(userObj.name + " has " +  + " heals remaining.");
-			}
-		} else if (takeTurn.toLowerCase() === "quit") {
-			return;
+function displayStats(){
+	document.getElementById("userNameEl").innerHTML = "Name: " + userObj.name;
+	document.getElementById("userHealthEl").innerHTML = "Health: " + userObj.health;
+	document.getElementById("userHealsEl").innerHTML = "Heals remaining: " + userObj.healsRemaining;
+	document.getElementById("userWinsEl").innerHTML = "User wins: " + userObj.wins;
+	document.getElementById("grantNameEl").innerHTML = "Opponent name: " + grantObj.name;
+	document.getElementById("grantHealthEl").innerHTML = "Opponent health: " + grantObj.health;
+	document.getElementById("attackBtnEl").innerHTML = "<button>Attack</button>";
+	document.getElementById("healBtnEl").innerHTML = "<button>Heal</button>";
+	document.getElementById("quitBtnEl").innerHTML = "<button>Quit</button>";
+}
+
+document.getElementById("attackBtnEl").addEventListener("click", function() {
+	if ((userObj.health) > 0 && userObj.wins < 5) {
+		grantObj.health -= userObj.generateAttackDamage();
+		userObj.health -= grantObj.generateAttackDamage();
+		console.log(userObj.name + " has " + userObj.health + " health left.");
+		console.log("Grant the almighty has " + grantObj.health + " health left.");
+		if (grantObj.health <= 0) {
+			userObj.wins += 1;
+			grantObj.health = 10;
+			// console.log("grant's health reset to " + grantHealth);
+			console.log(userObj.name + " has " + userObj.wins + " wins! You need " + (5 - userObj.wins) + " more wins!");
+		}
+		if (userObj.health <= 0) {
+			console.log("Grant is the winner!");
+		}
+		if (userObj.wins >= 5) {
+			console.log(userObj.name + " is the winner!");
 		}
 	}
-}
+});
 
-startGame();
+document.getElementById("healBtnEl").addEventListener("click", function() {
+	if (userObj.healsRemaining > 0) {
+		userObj.health += userObj.heal();
+		console.log("heals remaining: " + userObj.healsRemaining);
+		console.log("user health: " + userObj.health);
+	}
+});
+
+document.getElementById("quitBtnEl").addEventListener("click", function() {
+	console.log("quit!");
+	document.getElementById("gameContainerEl").classList.add("hidden");
+	document.getElementById("quitMessageEl").innerHTML = "<h1>You have quit the game! Refresh the page to play again.</h1>";
+});
+
+
 
 
