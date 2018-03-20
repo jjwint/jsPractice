fbd9f2da5498ca2e5b95b5e85afb3c0044564e7d
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-03-19 21:13:58 -0400

    cleaning up code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//User variables
 var userObj = {
 	name: null,
 	health: 40,
@@ -12,6 +13,7 @@ userObj.heal = function() {
 	return healAmount = Math.floor((Math.random() * 10) + 1);
 };
 
+//Opponent varaibles
 var grantObj = {
 	name: "Grant the Almighty",
 	health: 10
@@ -30,36 +32,35 @@ document.getElementById("startBtn").addEventListener("click", function() {
 });
 
 function startGame() {
-	inviteUser = prompt("Would you like to play a game with two characters?");
-	if (inviteUser.toLowerCase() === "yes") {
-		userObj.name = prompt("What would you like to name your character?");
-		displayStats();
-	}
-}
+	userObj.name = prompt("What would you like to name your character?");
+	displayStats();
+};
 
 function displayStats(){
 	document.getElementById("userNameEl").innerHTML = "Name: " + userObj.name;
 	document.getElementById("userHealthEl").innerHTML = "Health: " + userObj.health;
 	document.getElementById("userHealsEl").innerHTML = "Heals remaining: " + userObj.healsRemaining;
-	document.getElementById("userWinsEl").innerHTML = "User wins: " + userObj.wins;
+	document.getElementById("userWinsEl").innerHTML = "Wins: " + userObj.wins;
 	document.getElementById("grantNameEl").innerHTML = "Opponent name: " + grantObj.name;
 	document.getElementById("grantHealthEl").innerHTML = "Opponent health: " + grantObj.health;
 	document.getElementById("attackBtnEl").innerHTML = "<button>Attack</button>";
 	document.getElementById("healBtnEl").innerHTML = "<button>Heal</button>";
 	document.getElementById("quitBtnEl").innerHTML = "<button>Quit</button>";
-}
+};
 
 document.getElementById("attackBtnEl").addEventListener("click", function() {
 	if ((userObj.health) > 0 && userObj.wins < 5) {
 		grantObj.health -= userObj.generateAttackDamage();
 		userObj.health -= grantObj.generateAttackDamage();
-		console.log(userObj.name + " has " + userObj.health + " health left.");
-		console.log("Grant the almighty has " + grantObj.health + " health left.");
+		// console.log(userObj.name + " has " + userObj.health + " health left.");
+		document.getElementById("userHealthEl").innerHTML = "Health: " + userObj.health;
+		// console.log("Grant the almighty has " + grantObj.health + " health left.");
+		document.getElementById("grantHealthEl").innerHTML = "Opponent health: " + grantObj.health;
 		if (grantObj.health <= 0) {
 			userObj.wins += 1;
 			grantObj.health = 10;
-			// console.log("grant's health reset to " + grantHealth);
-			console.log(userObj.name + " has " + userObj.wins + " wins! You need " + (5 - userObj.wins) + " more wins!");
+			// console.log(userObj.name + " has " + userObj.wins + " wins! You need " + (5 - userObj.wins) + " more wins!");
+			document.getElementById("userWinsEl").innerHTML = "Wins: " + userObj.wins;
 		}
 		if (userObj.health <= 0) {
 			console.log("Grant is the winner!");
@@ -73,8 +74,10 @@ document.getElementById("attackBtnEl").addEventListener("click", function() {
 document.getElementById("healBtnEl").addEventListener("click", function() {
 	if (userObj.healsRemaining > 0) {
 		userObj.health += userObj.heal();
-		console.log("heals remaining: " + userObj.healsRemaining);
-		console.log("user health: " + userObj.health);
+		// console.log("heals remaining: " + userObj.healsRemaining);
+		document.getElementById("userHealsEl").innerHTML = "Heals remaining: " + userObj.healsRemaining;
+		// console.log("user health: " + userObj.health);
+		document.getElementById("userHealthEl").innerHTML = "Health: " + userObj.health;
 	}
 });
 
